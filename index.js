fa7c7eda870259a39a824f8c044407d6508af95b
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-18 21:17:11 +0300

    как получить ID интервала из функции

--- index.js
+++ index.js
@@ -8,28 +8,31 @@ const refs = {
 };
 
 refs.start.addEventListener('click', onChangeColor);
-
+refs.stop.addEventListener('click', stopChangeColor);
 // console.log(refs.colorBody);
 
 function onChangeColor() {
-  let num = randomIntegerFromInterval(0, 5);
-  
-  const intervalID = setInterval(() => {
-    refs.colorBody.setAttribute('style', `background-color:${colors[num]}`)
+  // let numID = intervalID;
+ const intervalID = setInterval(() => {
+    getRandomCollor()
   }, 1000);
-  
-  // if (intervalID) {
-  //   clearInterval(intervalID);
-  // };
-
+  if (intervalID) {
+    refs.start.setAttribute('disabled', true);
+  };
+  // return numID;
 };
 
+function getRandomCollor() {
+    let num = randomIntegerFromInterval(0, 5);
+    refs.colorBody.setAttribute('style', `background-color:${colors[num]}`);
+};
 
-
-
-//  refs.stop.addEventListener('click', clearInterval(intervalID));
-
-
+// console.log(numID);
+  
+function stopChangeColor() {
+  clearInterval(intervalID);
+  refs.start.removeAttribute('disabled');
+};
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
