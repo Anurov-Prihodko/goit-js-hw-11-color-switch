67bf66d6d787dd96c836c55dd9e59a1ac2fbccf6
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-18 15:15:36 +0300

    start

---
+++ index.js
@@ -0,0 +1,10 @@
+import { colors } from './src/colors.js'
+
+
+
+
+console.log(colors);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
