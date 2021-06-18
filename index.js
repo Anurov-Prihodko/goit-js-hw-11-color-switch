848be0b556b9d53a273153e886cef4dd18ac458b
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-18 17:42:07 +0300

    разметка

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 import { colors } from './src/colors.js'
+// console.log(colors);
+
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+  colorBody: document.body,
+};
+
+refs.start.addEventListener('click', onChangeColor);
+
+// console.log(refs.colorBody);
+
+function onChangeColor() {
+  let num = randomIntegerFromInterval(0, 5);
+  
+  const intervalID = setInterval(() => {
+    refs.colorBody.setAttribute('style', `background-color:${colors[num]}`)
+  }, 1000);
+  
+  // if (intervalID) {
+  //   clearInterval(intervalID);
+  // };
+
+};
 
 
 
 
-console.log(colors);
+//  refs.stop.addEventListener('click', clearInterval(intervalID));
+
+
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
